a62573c7f529016cd57bdc80ed1ab6819afcd2ab
lichao <lichao@gagougroup.com>
2018-12-15 14:43:04 +0800

    t1

---
+++ node_api_study/child_process/test1/child.js
@@ -0,0 +1,30 @@
+setTimeout(() => process.exit(0), 1000);
+
+
+// process.on("message", function(data) {
+//   console.log("收到: ", data);
+//   setTimeout(() => process.send({code: 1, msg: "finish"}), 1000 * 1);
+// });
+
+// process.send("ffuck");
+
+// console.log("sdfsdf")
+
+// var fs = require('fs');
+
+// console.log("fuck you");
+
+
+
+
+// let read_stream = fs.createReadStream(fs_index);
+
+
+
+// let write_stream = fs.createWriteStream(fs_index);
+
+
+
+
+
+
