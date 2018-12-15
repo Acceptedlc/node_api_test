68b46c8d9c1bc93eda1b7e42eac1aacb9a12fc1a
lichao <lichao@gagougroup.com>
2018-12-15 14:55:35 +0800

    t2

---
+++ node_api_study/child_process/test2/child.js
@@ -0,0 +1,31 @@
+console.log("child hello world");
+setTimeout(() => process.exit(0), 500);
+
+
+// process.on("message", function(data) {
+//   console.log("收到: ", data);
+//   setTimeout(() => process.send({code: 1, msg: "finish"}), 1000 * 1);
+// });
+
+// process.send("ffuck");
+
+// console.log("sdfsdf")
+
+// var fs = require('fs');
+
+// console.log("fuck you");
+
+
+
+
+// let read_stream = fs.createReadStream(fs_index);
+
+
+
+// let write_stream = fs.createWriteStream(fs_index);
+
+
+
+
+
+
---
+++ node_api_study/child_process/test2/parent.js
@@ -0,0 +1,21 @@
+var spawn = require('child_process').spawn;
+
+let child = spawn('node', [`${__dirname}/child.js`], {
+  stdio: ['ignore', 'inherit', 'ignore'],
+  detached: true,
+  cwd: __dirname
+});
+
+child.on('exit', function() {
+  console.log("child die");
+})
+
+setTimeout(() => console.log("i am die"), 1000 * 3600);
+
+// child.send("叫爸爸");
+
+// child.on("message", function(data) {
+//   console.log(data);
+// });
+
+// setTimeout(() => console.log("sfdsfsdf"), 1000 * 10000);
