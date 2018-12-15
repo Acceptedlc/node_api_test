a0229efc70348653b4fc046ec7700df7347f2d71
lichao <lichao@gagougroup.com>
2018-12-15 16:12:01 +0800

    feat: t2

--- node_api_study/child_process/test2/child.js
+++ node_api_study/child_process/test2/child.js
@@ -1,22 +1,21 @@
-console.log("fuck");
+const scope_parser_1 = require("scope-parser");
+
 process.on("message", function(data) {
-  console.log("child recv: ", data);
-  setTimeout(() => process.exit(0), 500);
+    scope_parser_1.setConfig(data.conf);
+    console.log(data.params.upLeft);
+    console.log(data.params.downRight);
+
+    scope_parser_1.Carbin.fire4Export(
+      JSON.parse(data.params.scopes), 
+      data.params.upLeft, 
+      data.params.downRight, 
+      data.fileName, 
+      data.params.crs, 
+      data.params.resolution, 
+      data.params.dataType, 
+      data.params.noDataValue)
+    .then(path => setImmediate(() => process.send({ code: 1 })))
+    .catch((e) => {
+      setImmediate(() => process.send({ code: 2, msg: e.message }));
+    });
 })
-
-
-
-
-
-
-// let read_stream = fs.createReadStream(fs_index);
-
-
-
-// let write_stream = fs.createWriteStream(fs_index);
-
-
-
-
-
-
